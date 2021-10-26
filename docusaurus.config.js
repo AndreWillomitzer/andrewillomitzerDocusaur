6da8e7e75a75760bd2fe31666b03741d7890cc2d
Andre Willomitzer <andre.willomitzer@gmail.com>
2021-10-26 19:52:20 -0400

    baseURL change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://andrewillomitzer.github.io',
-  baseUrl: '/andrewillomitzerdocuSSG/',
+  baseUrl: '/andrewillomitzerDocusaur/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
